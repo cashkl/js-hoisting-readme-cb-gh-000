fca90f3d1efc8939d8d03b2d6aa9d45df9a4d3e5
Murat Celik <cashkl@yandex.com>
2018-04-26 16:56:09 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -21,13 +21,14 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+
 
 
 
   function sayMy() {
+        var name = "Kristin";
     console.log(name);
-    var name = "Kristin";
+
   }
     sayMy();
 }
